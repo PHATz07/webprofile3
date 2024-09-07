ae1f03ca99bcf93d1735e2d2a4cbe96f5dca0d7e
PHATz07 <iamthanaphat.tt@gmail.com>
2024-09-07 15:32:29 +0700

    Add files via upload

---
+++ webprofile.py
@@ -0,0 +1,42 @@
+import streamlit as st
+
+# Page configuration
+st.set_page_config(page_title="Thanaphat Tuvirat", page_icon="🎶")
+
+# Title
+st.title("🎶 Who amI")
+
+# Introduction
+st.subheader("I'm Thanaphat Tuvirat (Phat)")
+st.write("""
+- 2024 - Cultural Diversity Workshop MUIC OPEN DAY 2024
+- 2024 - Debsirin First Aid and CPR Course
+- 2024 - K-Engineering World Tour and Workshop 2024
+- 2024 - Sensors and IOT systems in greenhouse Workshop KMITL 2024
+""")
+
+# Current Activities
+st.subheader("🧑‍💻 What I'm Doing Now")
+st.write("""
+- 📖 Studying in Debsirin School
+- 🔬 Debsirin Engineer Sci-Math class
+- 🤖 Learning about C language
+""")
+
+# Future Plans
+st.subheader("🔮 What in Future")
+st.write("""
+- [ ] Study Computer Engineer (2026-2029)
+""")
+
+# Contact Information
+st.subheader("📩 Connect with Me")
+st.write("""
+- 📩 iamthanaphat.tt@gmail.com
+- 🔗 [Facebook](https://www.facebook.com/profile.php?id=100043825202970)
+- 📖 [GitHub](https://github.com/PHATz07)
+""")
+
+# Footer
+st.write("Thank you  for reading 💕🤟")
+
